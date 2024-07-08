7542c99b7cbdac928946378c2ea099bdb2efc6fb
Sam Caldwell <github@samcaldwell.net>
2024-07-08 08:40:45 -0500

    add AllInteger and test

---
+++ AllInteger_test.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"testing"
+)
+
+func AnyIntegerFunc[T AllInteger](i T) bool {
+	return any(i) == any(i)
+}
+
+func TestAnyInteger(t *testing.T) {
+	assert := func(c bool) {
+		if !c {
+			t.Fatal("error")
+		}
+	}
+
+	assert(AnyIntegerFunc(int(0)))
+	assert(AnyIntegerFunc(int8(0)))
+	assert(AnyIntegerFunc(int16(0)))
+	assert(AnyIntegerFunc(int32(0)))
+	assert(AnyIntegerFunc(int64(0)))
+
+	assert(AnyIntegerFunc(uint(0)))
+	assert(AnyIntegerFunc(uint8(0)))
+	assert(AnyIntegerFunc(uint16(0)))
+	assert(AnyIntegerFunc(uint32(0)))
+	assert(AnyIntegerFunc(uint64(0)))
+}
